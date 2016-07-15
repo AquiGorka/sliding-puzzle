1e009d923cab964371d8ed7127000b51e50bf0ec
Gorka <gorka@aquigorka.com>
2016-07-15 09:45:09 -0300

    Adds tile movement

--- src/app.js
+++ src/app.js
@@ -1,48 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './app.styl';
 
 const Tile = (props) => {
-  const { item, dimensions, size, onClick } = props;
-  const { x, y, id } = item;
-  let myStyles = {
+  const { item, dimensions, size, onClick, tileIndex } = props,
+    { id } = item,
+    index = id,
+    x = index % size,
+    y = parseInt(index / size);
+  let last = {},
+    myStyles = {
       height: `${100 / size}%`,
       width: `${100 / size}%`,
       backgroundSize: `${dimensions}px ${dimensions}px`,
       backgroundPosition: `${100 / (size - 1) * x}% ${100 / (size - 1) * y}%`
     };
-  if (x === size - 1 && y === size - 1) {
+  if (index === (size * size) - 1) {
     myStyles.background = 'none';
+    last.display = 'none';
   }
   return (
-    <div className={styles.tile} style={myStyles} onClick={() => { onClick(id) }}>
-      <div className={styles['tile-id']}>{id}</div>
+    <div className={styles.tile} style={myStyles} onClick={() => {
+        if (index !== (size * size) - 1) {
+          onClick({ id, index: tileIndex });
+        }
+      }}>
+      <div className={styles['tile-id']} style={last}>{id + 1}</div>
     </div>
   );
 }
 //
 
-export default (props) => {
-  const { game, dimensions } = props,
-    { size, elements } = game,
-    myStyles = {
-      height: `${dimensions}px`,
-      width: `${dimensions}px`
-    };
-  let x = -1,
-    y = -1,
-    id = 0;
-  return (
-		<main className={styles.app} style={myStyles}>
-      <div className={styles.bg}></div>
-      <div className={styles.wrapper}>
-        {elements.map((item, index) =>
-          <Tile item={item} key={index} dimensions={dimensions} size={size} onClick={(id) => {
-            game.move(id);
-            if (game.won) {
-              alert('You have won!');
-            }
-          }} /> )}
-      </div>
-		</main>
-	);
-};
+export default class SlidingPuzzle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { moves: 0 };
+  }
+
+  componentDidUpdate() {
+    if (this.props.game.won) {
+      alert('You have won!');
+    }
+  }
+
+  render() {
+    const { game, dimensions } = this.props,
+      { size, elements } = game,
+      myStyles = {
+        height: `${dimensions}px`,
+        width: `${dimensions}px`
+      };
+    let x = -1,
+      y = -1,
+      id = 0;
+    return (
+      <main className={styles.app} style={myStyles}>
+        <div className={styles.bg}></div>
+        <div className={styles.wrapper}>
+          {elements.map((item, index) =>
+            <Tile item={item} key={index} tileIndex={index} dimensions={dimensions} size={size} onClick={({ id, index }) => {
+              this.setState({ moves: this.state.moves + 1 });
+              game.move({ id, index });
+            }} /> )}
+        </div>
+      </main>
+    );
+  }
+}
